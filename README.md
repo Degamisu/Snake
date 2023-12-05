0384de8369d00d00a20bc87500ec72a6c2a6e9be
Degamisu <degamisu@gmail.com>
2023-12-05 18:20:06 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Snake
- A genesis tech demo!
+# My Awesome Game
+
+This is My Awesome 16 Bit Cartridge game for mega Drive
---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <genesis.h>
+
+int main()
+{
+    VDP_drawText("Hello Sega!!", 10,13);
+    while(1)
+    {
+        //For versions prior to SGDK 1.60 use VDP_waitVSync instead.
+        SYS_doVBlankProcess();
+    }
+    return (0);
+}
